16614c1597b2350a13abd0279ea98826fec265e7
Rob Mosher <rob@robmosher.com>
2026-02-10 09:31:29 -0500

    chore: steady pacing of  "call today"

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './main.css'
 export default function App() {
   return (
     <div className="vignette flex h-screen flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("/elevatr-bg.jpg")' }}>
-      <Marquee isLeftToRight speedMultiplier={1.20} text="call today" />
+      <Marquee isLeftToRight speedMultiplier={0.5} text="call today" />
       <PhoneNumber />
       <Marquee isLeftToRight={false} speedMultiplier={0.5} text="or don't, we respect your agency" />
     </div>
