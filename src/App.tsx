5f6f6e505f64338b402f1b3d6e873a35d2d07575
Rob Mosher <rob@robmosher.com>
2026-02-10 09:29:35 -0500

    fix(copy): update marquee text to agency-respecting phrasing
    Replace "or else" with "or don't, we respect your agency" in the App marquee.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="vignette flex h-screen flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("/elevatr-bg.jpg")' }}>
       <Marquee isLeftToRight speedMultiplier={1.20} text="call today" />
       <PhoneNumber />
-      <Marquee isLeftToRight={false} speedMultiplier={0.5} text="or else" />
+      <Marquee isLeftToRight={false} speedMultiplier={0.5} text="or don't, we respect your agency" />
     </div>
   )
 }
